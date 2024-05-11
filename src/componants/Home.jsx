04cbeba9e4b06fd6152b0260d74d3c3e952628ff
safwan 169 <safwanhossainmridul@gmail.com>
2024-05-12 02:08:57 +0600

    sixth commit

--- src/componants/Home.jsx
+++ src/componants/Home.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import { SwiperSlide,Swiper } from 'swiper/react';
+import { SwiperSlide, Swiper } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import Recent from './Recent';
 const Home = () => {
 
     const data = useLoaderData()
 
-    const pp=data[0].date
-   
+    const pp = data[0].date
+
     const dateObject = new Date(pp).toLocaleString();
-  
 
 
-;
+
+    ;
 
 
-      const sortedData = data.sort((a, b) => (b.date-a.date ));
+    const sortedData = data.sort((a, b) => (b.date - a.date));
 
-    // const slicedData = data.slice(0, 6);
+    const slicedData = data.slice(0, 6);
     console.log(sortedData)
 
 
@@ -31,32 +32,43 @@ const Home = () => {
 
 
 
-    
+
     return (
-      
+
         <>
 
 //         {/* <Helmet>
 //             <title>Brite || Home</title>
 //         </Helmet> */}
 
-        <Swiper
-            spaceBetween={30}
-            autoplay={{
-                delay: 3000,
-            }}
-            
-            modules={[Autoplay ]}
-            className="mySwiper"
-        >
-            <SwiperSlide ><img className=' w-full bg-cover h-svh lg:h-[650px]' src="https://i.ibb.co/5R4Gpqy/1.jpg" alt="" /></SwiperSlide>
-            <SwiperSlide ><img className=' w-full bg-cover h-svh lg:h-[650px]' src="https://i.ibb.co/5R4Gpqy/1.jpg"alt="" /></SwiperSlide>
-            <SwiperSlide ><img className=' w-full bg-cover h-svh lg:h-[650px]' src="https://i.ibb.co/5R4Gpqy/1.jpg"alt="" /></SwiperSlide>
-            <SwiperSlide ><img className=' w-full bg-cover h-svh lg:h-[650px]' src="https://i.ibb.co/5R4Gpqy/1.jpg" alt="" /></SwiperSlide>
-            
+            <Swiper
+                spaceBetween={30}
+                autoplay={{
+                    delay: 3000,
+                }}
 
-        </Swiper>
-//      
+                modules={[Autoplay]}
+                className="mySwiper"
+            >
+                <SwiperSlide ><img className=' w-full bg-cover h-svh lg:h-[650px]' src="https://i.ibb.co/5R4Gpqy/1.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide ><img className=' w-full bg-cover h-svh lg:h-[650px]' src="https://i.ibb.co/5R4Gpqy/1.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide ><img className=' w-full bg-cover h-svh lg:h-[650px]' src="https://i.ibb.co/5R4Gpqy/1.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide ><img className=' w-full bg-cover h-svh lg:h-[650px]' src="https://i.ibb.co/5R4Gpqy/1.jpg" alt="" /></SwiperSlide>
+
+
+            </Swiper>
+            <div>
+                <div>
+                    Recent Queries
+                </div>
+                <div className="lg:grid  lg:grid-cols-3 justify-center gap-4 w-full lg:w-[1200px] mx-auto my-10">
+
+                    {
+                        slicedData.map((d, id) => <Recent idx={id} datas={d}></Recent>)
+                    }
+                </div>
+            </div>
+//
         </>
 
     );
---
+++ src/componants/Recent.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import 'animate.css'
+import { IoTimeOutline } from 'react-icons/io5';
+import { CiLocationOn } from 'react-icons/ci';
+const Recent = ({ datas }) => {
+    console.log(datas)
+    const { url, name, reason, product_title, email, image, date, brand_name, user_name } = datas
+    const datee = new Date(date).toLocaleString()
+    return (
+        <div>
+
+
+            <div className="max-w-sm my-5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img className="rounded-t-lg w-[400px] h-[300px]" src={`${url}`} alt="" />
+                </a>
+                <div className="p-5">
+                    <a href="#">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{product_title}</h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{name}</p>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Brand : {brand_name}</p>
+                    <p className="mb-3 font-normal h-10 text-gray-700 dark:text-gray-400">Reason : {reason}</p>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Post : {datee}</p>
+                    <p className="mb-3 font-normal text-gray-700 flex items-center gap-10 dark:text-gray-400"><span><img className='w-16 rounded-xl h-16' src={image} alt="" /></span><span>{user_name}</span></p>
+                    {/* <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Read more
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </a> */}
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Recent;
